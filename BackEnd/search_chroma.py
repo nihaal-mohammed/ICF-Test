708b92e7f6c090406987db983fae28f754f0a7dd
fatimrahm <fatima.ahmed@icf-edu.org>
2025-07-22 22:30:42 -0500

    search chroma db

---
+++ BackEnd/search_chroma.py
@@ -0,0 +1,41 @@
+import chromadb
+
+from BackEnd.vector_db_pipline import vectorize_text_segments
+
+
+def search_chroma(
+    query: str,
+):
+    """
+    Uploads vector embeddings along with their original documents into a persistent Chroma database.
+
+    Args:
+        embeddings (List[List[float]]): List of embedding vectors.
+        documents (List[str]): List of original text documents.
+        ids (List[str]): Unique identifiers for each document.
+
+    Returns:
+        None
+    """
+    try:
+        # ✅ Initialize persistent ChromaDB client
+        client = chromadb.PersistentClient(path="chroma")
+
+        # ✅ Create or connect to a collection
+        collection = client.get_or_create_collection(name="frisco_events")
+
+        # embed query
+        query_embeddings = vectorize_text_segments([query])[0]
+        # ✅ Upload documents, embeddings, and their IDs
+        results = collection.query(query_embeddings=query_embeddings, n_results=5)
+
+        print(
+            f"[✓] Search results for query '{query}': {len(results['documents'])} found."
+        )
+        return results["documents"], results["ids"]
+
+    except Exception as e:
+        print(f"[✗] Failed to upload to ChromaDB: {e}")
+
+
+print(search_chroma("events in frisco masjid"))
