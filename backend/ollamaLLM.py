ef258954637f473c6cdf230073b575e89851e460
testusermk-icf <Coder_Guy_882@protonmail.com>
2025-07-27 18:05:23 -0500

    added context to chatbot

--- backend/ollamaLLM.py
+++ backend/ollamaLLM.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-import re
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -16,37 +15,45 @@ app.add_middleware(
 )
 
 class AskRequest(BaseModel):
-    context: str = ''
     question: str
     history: list = []
+
+context = """Due to our extensive community growth over the past few years, Sunday School has adopted a two-shift schedule to accommodate additional students while maintaining a productive environment.
+
+Students in kindergarten to third grade are in the first shift, which is from 9:00 AM to 11:00 AM.
+
+Students in fourth to eighth grade are in the second shift, which is from 11:45 AM to 2:15 PM."""
 
 template = """
 Here is the conversation history:
 {history}
+
+Here is the context:
+{context}
 
 Now, answer the user's question: {question}
 
 Answer:
 """
 
-model = OllamaLLM(model="deepseek-r1")
+model = OllamaLLM(model="gemma3")
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 @app.post('/ask')
 async def ask(request: AskRequest):
-    context = request.context
     question = request.question
-    history = request.history
-    history.append(f"User: {question}")
+    history = request.history + [f"User: {question}"]
 
     try:
-        result = chain.invoke({"context": context, "question": question, "history": "\n".join(history)})
-        answer = re.split(r'</?think>', result)[-1]
-
+        answer = chain.invoke({
+            "context": context,
+            "question": question,
+            "history": "\n".join(history)
+        })
         history.append(f"Bot: {answer}")
-        
         return {'answer': answer, 'history': history}
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
